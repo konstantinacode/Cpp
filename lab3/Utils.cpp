863d381a8766f112702b2ce6a585e601f97845ac
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-10-26 21:27:35 +0300

    Update Utils.cpp

--- lab3/Utils.cpp
+++ lab3/Utils.cpp
@@ -21,3 +21,44 @@ void printStringVector (const std::vector<std::string> &vector) {
 	std::cout << std::endl;
 }
 
+void printStringSet (const std::set<std::string> &set) {
+	// TODO Write the code to print the data of set container
+	std::set<std::string>::const_iterator cit;
+
+	std::cout << std::endl;
+	std::cout << "Strings contained in the set:" << std::endl;
+	std::cout << "-----------------------------" << std::endl;
+
+	for (cit = set.begin(); cit != set.end(); ++cit) {
+		std::cout << *cit << std::endl;
+	}
+
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << std::endl;
+}
+
+bool isDigit(const std::string &word) {
+	bool retVal = true;
+
+	for(std::string::const_iterator k = word.begin(); k != word.end(); ++k)
+		retVal = retVal && isdigit(*k);
+
+	return retVal;
+}
+
+int readPosInt ( ) {
+	std::string input;
+	bool validInput = false;
+	std::cout << "Type a positive integer value :: ";
+	do {
+		std::cin >> input;
+
+		if ( !(validInput = isDigit(input)) ) {
+			std::cout << input << " is not a number! Try again.\n";
+		}
+		else if (atoi(input.c_str()) <= 0) {
+				std::cout << input << " Is not a positive integer! Try again\n";
+		}
+	} while ( !validInput );
+	return atoi(input.c_str());
+}
