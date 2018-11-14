e3070cc258a91980a7f2dc53d2c0e9938d97615e
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-11-14 21:41:56 +0200

    Update Utils.cpp

--- lab3/Utils.cpp
+++ lab3/Utils.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <string>  
 
-void printStringVector (const std::vector<std::string> &vector) {
-	// Use an iterator to print the contents of the vector.
-	// The iterator is const_iterator which means that we 
-	// cannot use it to change the contents of the set.
-	std::vector<std::string>::const_iterator cit;
-	
-	std::cout << std::endl;
-	std::cout << "Strings contained in the vector:" << std::endl;
-	std::cout << "--------------------------------" << std::endl;
-
-	for (cit = vector.begin(); cit != vector.end(); ++cit) {
-		std::cout << *cit << std::endl;
-	}
-
-	std::cout << "--------------------------------" << std::endl;
-	std::cout << std::endl;
-}
-
-void printStringSet (const std::set<std::string> &set) {
-	// TODO Write the code to print the data of set container
-	std::set<std::string>::const_iterator cit;
-
-	std::cout << std::endl;
-	std::cout << "Strings contained in the set:" << std::endl;
-	std::cout << "-----------------------------" << std::endl;
-
-	for (cit = set.begin(); cit != set.end(); ++cit) {
-		std::cout << *cit << std::endl;
-	}
-
-	std::cout << "-----------------------------" << std::endl;
-	std::cout << std::endl;
-}
 
 bool isDigit(const std::string &word) {
 	bool retVal = true;
