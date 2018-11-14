8ab51ae78b05602e64179a3a40f6b2c7b58d368d
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-11-14 21:42:37 +0200

    Update Demos.h

--- lab3/Demos.h
+++ lab3/Demos.h
@@ -1,18 +1,13 @@
 #ifndef DEMOS_H_
 #define DEMOS_H_
-
 #include <string>
 #include <vector>
-#include <set>
 
 // Declarations
 void string_examples();
-
 void simpleFileWriteExample ( void );
 void simpleReadExample ( const char *fName );
-
 void readWriteBinaryExample ( void );
 
-int readInputFile ( char *inputFile, std::vector<std::string> vContainer, std::set<std::string> sContainer );
 
 #endif	// DEMOS_H_
