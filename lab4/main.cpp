b6c7e3c340296ba7c839687d2ded6db7680ec6aa
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-11-15 18:48:45 +0200

    Create main.cpp

---
+++ lab4/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+
+#include "Utils.h"
+#include "Demos.h"
+
+using namespace std;
+
+//#define STANDARD_IO
+//#define STRINGS
+
+int main (int argc, char *argv[]) {
+#ifdef STANDARD_IO
+	// cout -- The Standard Output Stream
+	cout << "Hello from cout" << endl;
+	// cerr -- The Standard Output Stream for errors
+	cerr << "Hello from cerr" << endl;
+	// cin  -- The Standard Input Stream
+	int x;
+	cout << "Write an int: ";
+	cin >> x;
+	cout << "You typed: " << x << endl;
+
+	cout << "Give 2 int values and 1 double: ";
+	// TODO: Write the code and print the values
+	int a, b; double d;
+	cin >> a >> b >> d;
+	cout << "a: " << a << " | b: " << b << " | d: " << d << endl;
+	
+	cout << "Give text: ";
+	// TODO: Get text from input and print it
+	string text;
+	cin >> text;
+	cout << "T: " << text << endl;
+
+	cout << "Write a line ::\n";
+	char buf[256];
+	cin.ignore();	// Used to skip the \n (Enter) pressed by the user in the previous input
+	cin.getline(buf,256);
+	cout << "You typed :: " << buf << endl;
+	string line;
+	cout << "Write a line again ::\n";
+	getline(cin,line);
+	cout << "You typed :: " << line << endl;
+	system("PAUSE");
+#elif defined STRINGS
+	string_examples();
+	system("PAUSE");
+#else
+	if (argc != 2) {
+		cout << "Usage: Missing input_file" << endl;
+		system("PAUSE");
+		return -1;
+	}
+
+	// Data Structures
+	vector <std::string> vNamesList;
+	
+
+	if (!readInputFile (argv[1], vNamesList)) {
+		system("PAUSE");
+		return -1;
+	}
+
+	// Now print the strings contained in Vector
+	printStringVector(vNamesList);
+	// Now print the strings contained in Set
+	system("PAUSE");
+	
+#endif
+
+	return 0;
+}
