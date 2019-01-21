0fe9c7bc112bc8d3cb09de6e5bbbd1d5c5d7f368
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2019-01-21 16:51:07 +0200

    Create Demos.h

---
+++ ReadWrite/Demos.h
@@ -0,0 +1,13 @@
+#ifndef DEMOS_H_
+#define DEMOS_H_
+#include <string>
+#include <vector>
+
+// Declarations
+void string_examples();
+void simpleFileWriteExample ( void );
+void simpleReadExample ( const char *fName );
+void readWriteBinaryExample ( void );
+
+
+#endif	// DEMOS_H_
