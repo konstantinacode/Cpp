9d0a92ed6f36a31dc572bb962fcfce7d848107f3
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2019-01-31 17:43:31 +0200

    Create Demos.h

---
+++ library/Demos.h
@@ -0,0 +1,11 @@
+#ifndef DEMOS_H_
+#define DEMOS_H_
+
+#include <string>
+#include <vector>
+
+// Declarations
+void string_examples();
+int readInputFile ( char *inputFile, std::vector<std::string> &vContainer);
+
+#endif	// DEMOS_H_
